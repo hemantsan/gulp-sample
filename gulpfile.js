dcb2fbcc5baf05ca04db08de6a7daf7ce9c5405f
Hemant Sankhla <sankhlahemant251@gmail.com>
2018-05-18 18:35:26 +0530

    code refactoring for minifiying css and js, implemented injecting assests into html files.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,17 @@ var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
+var inject = require('gulp-inject');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+
+let jsStream = [
+        './app/js/jquery-3.3.1.slim.min.js',
+        './app/js/popper.min.js',
+        './app/js/owl.carousel.min.js',
+        './app/js/**/*.js'
+    ],
+    cssStream = './app/css/**/*.css';
 
 gulp.task('sass', () => {
     return gulp
@@ -35,21 +46,34 @@ gulp.task('browserSync', () => {
 
 gulp.task('minify_js', () =>
     gulp
-        .src('app/js/app.js')
+        .src(jsStream)
         .pipe(
             babel({
-                presets: ['env']
+                presets: [
+                    [
+                        'env',
+                        {
+                            loose: true,
+                            modules: false,
+                            exclude: ['transform-es2015-typeof-symbol']
+                        }
+                    ]
+                ]
             })
         )
+        .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'))
 );
 
 gulp.task('minify_css', function() {
-    return gulp
-        .src('app/css/styles.css')
-        .pipe(cssnano())
-        .pipe(gulp.dest('dist/css'));
+    return (
+        gulp
+            .src(cssStream)
+            // .pipe(concatCss('site.css'))
+            .pipe(cssnano())
+            .pipe(gulp.dest('dist/css'))
+    );
 });
 
 gulp.task('optimize_images', function() {
@@ -63,6 +87,29 @@ gulp.task('optimize_images', function() {
         .pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('build', ['minify_css', 'minify_js', 'optimize_images'], function() {
-    return gulp.src('app/*.html').pipe(gulp.dest('dist'));
+gulp.task('build', ['minify_css', 'minify_js'], function() {
+    return gulp
+        .src('app/*.html')
+        .pipe(
+            inject(
+                gulp.src(['./dist/css/**/*.css', './dist/js/**/*.js'], {
+                    read: false
+                }),
+                {
+                    relative: true
+                }
+            )
+        )
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('inject_assets', () => {
+    gulp
+        .src('./app/**/*.html')
+        .pipe(
+            inject(gulp.src(jsStream.concat(cssStream), { read: false }), {
+                relative: true
+            })
+        )
+        .pipe(gulp.dest('./app'));
 });
